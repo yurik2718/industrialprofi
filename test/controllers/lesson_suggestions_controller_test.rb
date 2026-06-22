cf101789187491e626b38914da4e05fce8d1254c
Andrey Yurik <3swyylxo@anonaddy.com>
2026-06-22 06:33:14 +0300

    fix registration

--- test/controllers/lesson_suggestions_controller_test.rb
+++ test/controllers/lesson_suggestions_controller_test.rb
@@ -1,10 +1,18 @@
 require "test_helper"
 
 class LessonSuggestionsControllerTest < ActionDispatch::IntegrationTest
+  setup { sign_in_as users(:member) }
+
   test "new returns success" do
     get new_lesson_suggestion_path(lesson_slug: lessons(:pteep).slug)
     assert_response :success
     assert_match lessons(:pteep).title, response.body
+  end
+
+  test "signed-out visitors are sent to sign in" do
+    sign_out
+    get new_lesson_suggestion_path(lesson_slug: lessons(:pteep).slug)
+    assert_redirected_to new_session_path
   end
 
   test "create with valid data redirects with notice" do
@@ -12,9 +20,7 @@ class LessonSuggestionsControllerTest < ActionDispatch::IntegrationTest
       post lesson_suggestions_path(lessons(:pteep)), params: {
         lesson_suggestion: {
           section: "body",
-          body_markdown: "Improved content here",
-          author_name: "Test User",
-          author_contact: "test@example.com"
+          body_markdown: "Improved content here"
         }
       }
     end
@@ -22,12 +28,28 @@ class LessonSuggestionsControllerTest < ActionDispatch::IntegrationTest
     assert_equal I18n.t("flash.suggestion_submitted"), flash[:notice]
   end
 
+  test "create signs the suggestion with the current user's name" do
+    post lesson_suggestions_path(lessons(:pteep)), params: {
+      lesson_suggestion: { section: "body", body_markdown: "Improved content here" }
+    }
+    assert_equal users(:member).name, LessonSuggestion.order(:created_at).last.author_name
+  end
+
+  test "signed-out visitors cannot create" do
+    sign_out
+    assert_no_difference "LessonSuggestion.count" do
+      post lesson_suggestions_path(lessons(:pteep)), params: {
+        lesson_suggestion: { section: "body", body_markdown: "Improved content here" }
+      }
+    end
+    assert_redirected_to new_session_path
+  end
+
   test "create captures the edit reason and a base snapshot" do
     post lesson_suggestions_path(lessons(:pteep)), params: {
       lesson_suggestion: {
         section: "body",
         body_markdown: "Improved content here",
-        author_name: "Test User",
         edit_reason: "Поправил опечатку"
       }
     }
@@ -36,24 +58,11 @@ class LessonSuggestionsControllerTest < ActionDispatch::IntegrationTest
     assert_includes suggestion.base_content, "Содержание урока по ПТЭЭП"
   end
 
-  test "create without author_name re-renders form" do
-    assert_no_difference "LessonSuggestion.count" do
-      post lesson_suggestions_path(lessons(:pteep)), params: {
-        lesson_suggestion: {
-          body_markdown: "Content",
-          author_name: ""
-        }
-      }
-    end
-    assert_response :unprocessable_entity
-  end
-
   test "create without body_markdown re-renders form" do
     assert_no_difference "LessonSuggestion.count" do
       post lesson_suggestions_path(lessons(:pteep)), params: {
         lesson_suggestion: {
-          body_markdown: "",
-          author_name: "Author"
+          body_markdown: ""
         }
       }
     end
@@ -66,8 +75,7 @@ class LessonSuggestionsControllerTest < ActionDispatch::IntegrationTest
         company: "spam-bot",
         lesson_suggestion: {
           section: "body",
-          body_markdown: "Spam content",
-          author_name: "Bot"
+          body_markdown: "Spam content"
         }
       }
     end
